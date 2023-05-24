d65fb325c56e591e9a2923056bf60ba6ff043468
Ashli Katt <sqrtseven7@gmail.com>
2023-05-24 10:21:17 -0500

    Item support

--- src/main/kotlin/transpiler/values/Item.kt
+++ src/main/kotlin/transpiler/values/Item.kt
@@ -1,14 +1,20 @@
 package transpiler.values
 
+import MalformedList
 import Value
+import serializer.serializeString
 import transpiler.CheckContext
+import transpiler.checkList
+import transpiler.checkStr
 
-data class Item(val data: String) : DFValue {
+data class Item(val nbt: String) : DFValue {
     companion object {
         fun transpileFrom(input: Value, context: CheckContext): Item {
-            TODO("Not yet implemented")
+            val inpList = checkList(input)
+            if (inpList.size != 2) throw MalformedList("Value", "(item String<NBT>)", input)
+            return Item(checkStr(inpList[1]))
         }
     }
-    override fun serialize() = "{}" // TODO
-    override fun toString() = "{ITEM}"
+    override fun serialize() = """{"id":"item","data":{"item":${serializeString(nbt)}}}"""
+    override fun toString() = "SOME_ITEM"
 }
