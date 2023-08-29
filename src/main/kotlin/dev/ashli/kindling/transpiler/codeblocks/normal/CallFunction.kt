ef6bab8b6e04fd5c03fa5d76d816abe57e13a8ba
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-08-29 15:48:39 -0400

    textless part 4.2 - add function call parameters

--- src/main/kotlin/dev/ashli/kindling/transpiler/codeblocks/normal/CallFunction.kt
+++ src/main/kotlin/dev/ashli/kindling/transpiler/codeblocks/normal/CallFunction.kt
@@ -1,16 +1,19 @@
 package dev.ashli.kindling.transpiler.codeblocks.normal
 
 import dev.ashli.kindling.serializer.serialize
+import dev.ashli.kindling.serializer.serializeArgs
+import dev.ashli.kindling.transpiler.values.DFValue
 
 /**
  * Represents DiamondFire's Call Function block.
- * @param name Name of the function to call.
+ * @param name Name of the function to call
+ * @param arguments Arguments to call function with
  */
-data class CallFunction(val name: String) : DFBlock("call_func", 2) {
+data class CallFunction(val name: String, val arguments: List<DFValue>) : DFBlock("call_func", 2) {
     override fun serialize() =  "{" +
             """"id":"block",""" +
             """"block":"call_func",""" +
-            """"args":{"items":[]},""" +
+            """"args":${serializeArgs(arguments)},""" +
             """"data":${name.serialize()}""" +
             "}"
 }
