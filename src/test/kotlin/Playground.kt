596428d4e3575c2ead9a7012579eddd7561d8b99
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-08-29 15:25:17 -0400

    textless part 4 - convert to library and add playground

---
+++ src/test/kotlin/Playground.kt
@@ -0,0 +1,24 @@
+import dev.ashli.kindling.DFProgram
+import dev.ashli.kindling.serializer.sendPackageRecode
+import dev.ashli.kindling.transpiler.Selector
+import dev.ashli.kindling.transpiler.codeblocks.header.PlayerEvent
+import dev.ashli.kindling.transpiler.codeblocks.normal.PlayerAction
+import dev.ashli.kindling.transpiler.values.StyledText
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class Playground {
+    @Test
+    fun playground() {
+        // Put playground code here
+        sendPackageRecode(DFProgram(
+            listOf(
+                PlayerEvent("Join", listOf(
+                    PlayerAction("SendMessage", Selector.ALL_PLAYERS, listOf(StyledText.fromMiniMessage("<blue>%default joined!")))
+                ))
+            )
+        ), 36, true)
+    }
+}
